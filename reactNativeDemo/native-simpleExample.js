189b35632f4da84542798134aadcac0834117679
Xin Gao <ubuntugx@hotmail.com>
2016-03-24 15:03:57 +0800

    Added files via upload

---
+++ reactNativeDemo/native-simpleExample.js
@@ -0,0 +1,25 @@
+import React,{
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+class AwesomeProject extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      
+    }
+  }
+  render(){
+    return ()
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
